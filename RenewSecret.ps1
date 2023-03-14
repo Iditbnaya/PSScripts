1744f7e3470274e7864747e47e49b9705191cec4
Idit Bnaya <47793710+Iditbnaya@users.noreply.github.com>
2023-03-14 14:08:21 +0200

    Azure - Renew Secret before exparation using PS

---
+++ RenewSecret.ps1
@@ -0,0 +1,38 @@
+﻿$Applications = Get-AzureADApplication -all $true
+
+$Days = 30
+$now = get-date
+
+    foreach ($app in $Applications) {
+        $AppName = $app.DisplayName
+        $AppID = $app.objectid 
+        $ApplID = $app.AppId
+        $secret = $App.PasswordCredentials
+        # $cert = $AppCreds.KeyCredentials
+             foreach ($s in $secret) {
+             $StartDate = $s.StartDate
+             $EndDate = $s.EndDate
+             $operation = $EndDate - $now
+             $ODays = $operation.Days
+             
+               if ($ODays -gt $Days)
+                {
+                #Renew Secret
+                     Remove-AzADAppCredential -ObjectId $AppID
+                      $newCredential = New-AzADAppCredential -ObjectId $AppID
+                     $newCredential
+                                                
+                }
+
+                else
+                {
+                    write-host "No need to renew" $AppName
+                }
+
+               }
+               }
+               
+
+
+             
+        
